94c81596e009836dac636d2643dda2700610df4f
Nadeem Johnson <johnsonepc@gmail.com>
2021-12-01 15:22:22 +0200

    finished development

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function sum() {
 
     let age = document.getElementById('age').value
 
-    let bmi = Math.round(weight / (height / 100) ** 2 * 10) /10;
+    let bmi = Math.round(weight / (height / 100) ** 2 * 10) / 10;
     document.getElementById('bmi').innerHTML = bmi
 
     if (document.getElementById('gender').value == 'male'){
@@ -15,11 +15,23 @@ function sum() {
     }
     else (document.getElementById('gender').value == 'female');{
         let idealBmi = Math.round(0.5 * weight / (height / 100) ** 2 + 0.03 * age + 11);
-        document.getElementById('ideal-bmi').innerHTML = idealBmi
+        document.getElementById('ideal-bmi').innerText = idealBmi
     }
 }
 
-
-
-
-
+function clearInputs(){
+    document.getElementById('weight').value = ''
+    document.getElementById('height').value = ''
+    document.getElementById('age').value = ''
+    document.getElementById('gender').value = ''
+    document.getElementById('bmi').innerHTML = ''
+    document.getElementById('ideal-bmi').innerHTML = ''
+}
+function disable(){
+    let lock = document.getElementById('gender').value;
+    if ( lock == 'male') {
+        document.getElementById('age').disabled = true;
+    }else{
+        document.getElementById('age').disabled = false;
+    }
+}
