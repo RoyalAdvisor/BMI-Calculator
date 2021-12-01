2e569d3c5ae5ba3da728375170e675c890471d44
Nadeem Johnson <johnsonepc@gmail.com>
2021-12-01 08:35:54 +0200

    First Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+function sum() {
+    let weight = document.getElementById('weight').value
+
+    let height = document.getElementById('height').value
+
+    let age = document.getElementById('age').value
+
+    let bmi = Math.round(weight / (height / 100) ** 2 * 10) /10;
+    document.getElementById('bmi').innerHTML = bmi
+
+    if (document.getElementById('gender').value == 'male'){
+        let idealBmi = Math.round(0.5 * weight / (height / 100) ** 2 + 11.5);
+        document.getElementById('ideal-bmi').innerHTML = idealBmi
+    }
+    else (document.getElementById('gender').value == 'female');{
+        let idealBmi = Math.round(0.5 * weight / (height / 100) ** 2 + 0.03 * age + 11);
+        document.getElementById('ideal-bmi').innerHTML = idealBmi
+    }
+}
+
+
+
+
+
